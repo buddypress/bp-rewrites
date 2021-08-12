94cf5627ffbb0f09a63022c3f9ad6443ae23a9a0
imath <math.viet@gmail.com>
2021-08-12 13:32:22 +0200

    Make sure the Messages component is active before resetting Notices hooks
    Fixes #2

--- src/bp-templates/bp-nouveau.php
+++ src/bp-templates/bp-nouveau.php
@@ -210,10 +210,12 @@ add_action( 'bp_nouveau_get_blogs_directory_nav_items', __NAMESPACE__ . '\bp_nou
  * @since ?.0.0
  */
 function bp_nouveau_reset_notifications_init_filters() {
-	remove_action( 'bp_init', 'bp_nouveau_notifications_init_filters', 20 );
-	add_action( 'bp_parse_query', 'bp_nouveau_notifications_init_filters', 20 );
-	remove_action( 'bp_init', 'bp_nouveau_push_sitewide_notices', 99 );
-	add_action( 'bp_parse_query', 'bp_nouveau_push_sitewide_notices', 99 );
+	if ( bp_is_active( 'messages' ) ) {
+		remove_action( 'bp_init', 'bp_nouveau_notifications_init_filters', 20 );
+		add_action( 'bp_parse_query', 'bp_nouveau_notifications_init_filters', 20 );
+		remove_action( 'bp_init', 'bp_nouveau_push_sitewide_notices', 99 );
+		add_action( 'bp_parse_query', 'bp_nouveau_push_sitewide_notices', 99 );
+	}
 }
 add_action( 'bp_init', __NAMESPACE__ . '\bp_nouveau_reset_notifications_init_filters', 1 );
 

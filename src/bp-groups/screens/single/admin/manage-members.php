1af8a196f441c3bd616de3b8a17cb468ac8a8541
imath <math.viet@gmail.com>
2021-08-17 14:43:05 +0200

    Make sure all Group Admin slugs can be customized

--- src/bp-groups/screens/single/admin/manage-members.php
+++ src/bp-groups/screens/single/admin/manage-members.php
@@ -19,7 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @since ?.0.0
  */
 function groups_screen_group_admin_manage_members() {
-	bp_core_redirect( bp_group_admin_rewrites_get_form_url( groups_get_current_group(), 'manage-members' ) );
+	bp_core_redirect( bp_group_admin_rewrites_get_form_url( groups_get_current_group(), 'manage-members', 'bp_group_manage_manage_members' ) );
 }
 add_action( 'groups_removed_member', __NAMESPACE__ . '\groups_screen_group_admin_manage_members', 1 );
 add_action( 'groups_unbanned_member', __NAMESPACE__ . '\groups_screen_group_admin_manage_members', 1 );

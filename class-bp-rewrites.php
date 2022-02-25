30daf9986e2af1c49f0d7c45c49ef17b2bc62702
imath <imathi.eu@outlook.fr>
2022-02-25 18:58:41 +0100

    Prevent a fatal error if BuddyPress is not active

--- class-bp-rewrites.php
+++ class-bp-rewrites.php
@@ -60,6 +60,28 @@ final class BP_Rewrites {
 	}
 
 	/**
+	 * Checks whether BuddyPress is active.
+	 *
+	 * @since 1.0.0
+	 */
+	public static function is_buddypress_active() {
+		$bp_plugin_basename   = 'buddypress/bp-loader.php';
+		$is_buddypress_active = false;
+		$sitewide_plugins     = (array) get_site_option( 'active_sitewide_plugins', array() );
+
+		if ( $sitewide_plugins ) {
+			$is_buddypress_active = in_array( $bp_plugin_basename, $sitewide_plugins, true );
+		}
+
+		if ( ! $is_buddypress_active ) {
+			$plugins              = (array) get_option( 'active_plugins', array() );
+			$is_buddypress_active = in_array( $bp_plugin_basename, $plugins, true );
+		}
+
+		return $is_buddypress_active;
+	}
+
+	/**
 	 * Toggle Directory pages post types.
 	 *
 	 * @since 1.0.0
@@ -72,11 +94,37 @@ final class BP_Rewrites {
 	}
 
 	/**
+	 * Displays an admin notice to explain how to install BP Rewrites.
+	 *
+	 * @since 1.0.0
+	 */
+	public static function admin_notice() {
+		if ( self::is_buddypress_active() ) {
+			return false;
+		}
+
+		$bp_plugin_link = sprintf( '<a href="%s">BuddyPress</a>', esc_url( _x( 'https://wordpress.org/plugins/buddypress', 'BuddyPress WP plugin directory URL', 'bp-rewrites' ) ) );
+
+		printf(
+			'<div class="notice notice-error is-dismissible"><p>%s</p></div>',
+			sprintf(
+				/* translators: 1. is the link to the BuddyPress plugin on the WordPress.org plugin directory. */
+				esc_html__( 'BP Rewrites requires the %1$s plugin to be active. Please deactivate BP Rewrites, activate %1$s and only then, reactivate BP Rewrites.', 'bp-rewrites' ),
+				$bp_plugin_link // phpcs:ignore
+			)
+		);
+	}
+
+	/**
 	 * Return an instance of this class.
 	 *
 	 * @since 1.0.0
 	 */
 	public static function start() {
+		// This plugin is only usable with BuddyPress.
+		if ( ! self::is_buddypress_active() ) {
+			return false;
+		}
 
 		// If the single instance hasn't been set, set it now.
 		if ( null === self::$instance ) {
@@ -99,9 +147,12 @@ function bp_rewrites() {
 }
 add_action( 'bp_loaded', __NAMESPACE__ . '\bp_rewrites', -1 );
 
-/**
+/*
  * Use Activation and Deactivation to switch directory pages post type between WP pages
  * and BuddyPress one.
  */
 register_activation_hook( __FILE__, array( __NAMESPACE__ . '\BP_Rewrites', 'toggle_post_types' ) );
 register_deactivation_hook( __FILE__, array( __NAMESPACE__ . '\BP_Rewrites', 'toggle_post_types' ) );
+
+// Displays a notice to inform BP Rewrites needs to be activated after BuddyPress.
+add_action( 'admin_notices', array( __NAMESPACE__ . '\BP_Rewrites', 'admin_notice' ) );

buddypress/bp-rewrites

commit 30daf9986e2af1c49f0d7c45c49ef17b2bc62702

Prevent a fatal error if BuddyPress is not active

class-bp-rewrites.php
@@ -60,6 +60,28 @@ final class BP_Rewrites {
 	}
 
 	/**
+	 * Checks whether BuddyPress is active.
+	 *
+	 * @since 1.0.0
+	 */
+	public static function is_buddypress_active() {
+		$bp_plugin_basename   = 'buddypress/bp-loader.php';
+		$is_buddypress_active = false;
+		$sitewide_plugins     = (array) get_site_option( 'active_sitewide_plugins', array() );
+
+		if ( $sitewide_plugins ) {
+			$is_buddypress_active = in_array( $bp_plugin_basename, $sitewide_plugins, true );
+		}
+
+		if ( ! $is_buddypress_active ) {
+			$plugins              = (array) get_option( 'active_plugins', array() );
+			$is_buddypress_active = in_array( $bp_plugin_basename, $plugins, true );
+		}
+
+		return $is_buddypress_active;
+	}
+
+	/**
 	 * Toggle Directory pages post types.
 	 *
 	 * @since 1.0.0
@@ -72,11 +94,37 @@ final class BP_Rewrites {
 	}
 
 	/**
+	 * Displays an admin notice to explain how to install BP Rewrites.
+	 *
+	 * @since 1.0.0
+	 */
+	public static function admin_notice() {
+		if ( self::is_buddypress_active() ) {
+			return false;
+		}
+
+		$bp_plugin_link = sprintf( '<a href="%s">BuddyPress</a>', esc_url( _x( 'https://wordpress.org/plugins/buddypress', 'BuddyPress WP plugin directory URL', 'bp-rewrites' ) ) );
+
+		printf(
+			'<div class="notice notice-error is-dismissible"><p>%s</p></div>',
+			sprintf(
+				/* translators: 1. is the link to the BuddyPress plugin on the WordPress.org plugin directory. */
+				esc_html__( 'BP Rewrites requires the %1$s plugin to be active. Please deactivate BP Rewrites, activate %1$s and only then, reactivate BP Rewrites.', 'bp-rewrites' ),
+				$bp_plugin_link // phpcs:ignore
+			)
+		);
+	}
+
+	/**
 	 * Return an instance of this class.
 	 *
 	 * @since 1.0.0
 	 */
 	public static function start() {
+		// This plugin is only usable with BuddyPress.
+		if ( ! self::is_buddypress_active() ) {
+			return false;
+		}
 
 		// If the single instance hasn't been set, set it now.
 		if ( null === self::$instance ) {
@@ -99,9 +147,12 @@ function bp_rewrites() {
 }
 add_action( 'bp_loaded', __NAMESPACE__ . '\bp_rewrites', -1 );
 
-/**
+/*
  * Use Activation and Deactivation to switch directory pages post type between WP pages
  * and BuddyPress one.
  */
 register_activation_hook( __FILE__, array( __NAMESPACE__ . '\BP_Rewrites', 'toggle_post_types' ) );
 register_deactivation_hook( __FILE__, array( __NAMESPACE__ . '\BP_Rewrites', 'toggle_post_types' ) );
+
+// Displays a notice to inform BP Rewrites needs to be activated after BuddyPress.
+add_action( 'admin_notices', array( __NAMESPACE__ . '\BP_Rewrites', 'admin_notice' ) );

inc/update.php
@@ -19,6 +19,10 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @since 1.0.0
  */
 function updater() {
+	if ( ! BP_Rewrites::is_buddypress_active() ) {
+		return false;
+	}
+
 	$directory_pages   = bp_core_get_directory_pages();
 	$nav_menu_item_ids = array();
 	$post_type         = 'buddypress';

languages/bp-rewrites-fr_FR.po
@@ -4,15 +4,15 @@ msgid ""
 msgstr ""
 "Project-Id-Version: BP Rewrites\n"
 "Report-Msgid-Bugs-To: https://github.com/buddypress/bp-rewrites/issues\n"
-"POT-Creation-Date: 2022-02-12T13:25:05+00:00\n"
-"PO-Revision-Date: 2022-02-12 14:28+0100\n"
+"POT-Creation-Date: 2022-02-25T14:58:09+00:00\n"
+"PO-Revision-Date: 2022-02-25 16:02+0100\n"
 "Last-Translator: \n"
 "Language-Team: ENGLISH <jjj@buddypress.org>\n"
 "Language: fr\n"
 "MIME-Version: 1.0\n"
 "Content-Type: text/plain; charset=UTF-8\n"
 "Content-Transfer-Encoding: 8bit\n"
-"X-Generator: Poedit 2.4.2\n"
+"X-Generator: Poedit 2.3.1\n"
 "X-Domain: bp-rewrites\n"
 "Plural-Forms: nplurals=2; plural=(n > 1);\n"
 
@@ -28,7 +28,7 @@ msgstr "https://github.com/buddypress/bp-rewrites"
 msgid "BuddyPress Rewrites development plugin."
 msgstr ""
 "Extension de développement de l’utilisation de la réécriture d’URL de "
-"BuddyPress"
+"BuddyPress."
 
 #. Author of the plugin
 msgid "The BuddyPress Community"
@@ -38,9 +38,23 @@ msgstr "La communauté BuddyPress"
 msgid "https://buddypress.org"
 msgstr "https://buddypress.org"
 
+#: class-bp-rewrites.php:106
+msgctxt "BuddyPress WP plugin directory URL"
+msgid "https://wordpress.org/plugins/buddypress"
+msgstr "https://fr.wordpress.org/plugins/buddypress"
+
+#. translators: 1. is the link to the BuddyPress plugin on the WordPress.org plugin directory.
+#: class-bp-rewrites.php:112
+msgid ""
+"BP Rewrites requires the %1$s plugin to be active. Please deactivate BP "
+"Rewrites, activate %1$s and only then, reactivate BP Rewrites."
+msgstr ""
+"BP Rewrites nécessite que l’extension %1$s soit active. Merci de désactiver "
+"BP Rewrites, d’activer %1$s et seulement alors, de réactiver BP Rewrites."
+
 #: inc/admin.php:85 inc/admin.php:86
 msgid "BuddyPress URLs"
-msgstr ""
+msgstr "URLs de BuddyPress"
 
 #: inc/functions.php:386
 msgctxt "Post Type label"
@@ -88,6 +102,12 @@ msgstr "Terminaison d’URL du répertoire"
 msgid "Single Member primary views slugs"
 msgstr "Terminaisons d’URL des vues principales du membre"
 
+#. translators: %s is the member primary view name
+#: src/bp-core/admin/bp-core-admin-rewrites.php:118
+msgctxt "member primary view name URL admin label"
+msgid "\"%s\" slug"
+msgstr "Terminaison d’URL de « %s »"
+
 #. translators: %s is the BP Component name the secondery views belong to.
 #: src/bp-core/admin/bp-core-admin-rewrites.php:138
 msgid "Single Member %s secondary views slugs"
@@ -115,7 +135,7 @@ msgstr "Terminaisons d’URL des vues visibles des administrateurs du groupe"
 #: src/bp-core/admin/bp-core-admin-rewrites.php:225
 msgctxt "group view name URL admin label"
 msgid "\"%s\" slug"
-msgstr "Terminaison d’URL « %s » "
+msgstr "Terminaison d’URL « %s »"
 
 #: src/bp-core/admin/bp-core-admin-rewrites.php:246
 msgid "Save Settings"

languages/bp-rewrites.pot
@@ -9,9 +9,9 @@ msgstr ""
 "MIME-Version: 1.0\n"
 "Content-Type: text/plain; charset=UTF-8\n"
 "Content-Transfer-Encoding: 8bit\n"
-"POT-Creation-Date: 2022-02-12T13:25:05+00:00\n"
+"POT-Creation-Date: 2022-02-25T14:58:09+00:00\n"
 "PO-Revision-Date: YEAR-MO-DA HO:MI+ZONE\n"
-"X-Generator: WP-CLI 2.5.0\n"
+"X-Generator: WP-CLI 2.5.0-alpha-1a19028\n"
 "X-Domain: bp-rewrites\n"
 
 #. Plugin Name of the plugin
@@ -32,6 +32,16 @@ msgstr ""
 
 #. Author URI of the plugin
 msgid "https://buddypress.org"
+msgstr ""
+
+#: class-bp-rewrites.php:106
+msgctxt "BuddyPress WP plugin directory URL"
+msgid "https://wordpress.org/plugins/buddypress"
+msgstr ""
+
+#. translators: 1. is the link to the BuddyPress plugin on the WordPress.org plugin directory.
+#: class-bp-rewrites.php:112
+msgid "BP Rewrites requires the %1$s plugin to be active. Please deactivate BP Rewrites, activate %1$s and only then, reactivate BP Rewrites."
 msgstr ""
 
 #: inc/admin.php:85
@@ -81,6 +91,12 @@ msgstr ""
 
 #: src/bp-core/admin/bp-core-admin-rewrites.php:98
 msgid "Single Member primary views slugs"
+msgstr ""
+
+#. translators: %s is the member primary view name
+#: src/bp-core/admin/bp-core-admin-rewrites.php:118
+msgctxt "member primary view name URL admin label"
+msgid "\"%s\" slug"
 msgstr ""
 
 #. translators: %s is the BP Component name the secondery views belong to.

commit e680f5cdf29d850b2b9ff5e55717127a4e7eb6a1

Make sure BP Rewrites loads when BuddyPress is network activated
https://github.com/buddypress/bp-rewrites/commit/30daf9986e2af1c49f0d7c45c49ef17b2bc62702 introduced a regression for multisite configs when BuddyPress is network activated. Reason is the option to store sitewide active plugins uses plugin basename as keys where the one to store active plugins do not.

Fixes #32

class-bp-rewrites.php
@@ -70,7 +70,7 @@ final class BP_Rewrites {
 		$sitewide_plugins     = (array) get_site_option( 'active_sitewide_plugins', array() );
 
 		if ( $sitewide_plugins ) {
-			$is_buddypress_active = in_array( $bp_plugin_basename, $sitewide_plugins, true );
+			$is_buddypress_active = isset( $sitewide_plugins[ $bp_plugin_basename ] );
 		}
 
 		if ( ! $is_buddypress_active ) {